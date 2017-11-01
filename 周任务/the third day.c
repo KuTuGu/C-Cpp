db5bb77d106b82ad70e5f02b0b485ad48bfbee54
玄夜 <32386869+KuTuGu@users.noreply.github.com>
2017-11-01 07:53:15 -0500

    Update the third day.c

--- 周任务/the third day.c
+++ 周任务/the third day.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 int main()
 {
-        int x,y,z=0,i=3,j=0;
-	for(;z<i;z++){
+    int x,y,z,i=3,j;
+	for(z=0;z<i;z++){
 		for(x=1;x<=z;x++)
 			putchar(' ');
 		for(y=1;y<=5-2*z;y++)
 			putchar('*');
 		putchar('\n');
 	}
-	for(;z<=i-1;z++,i--){
-		for(;i-2>0;i--)
+	for(z=0;z<=i-1;z++,i--){
+		for(i=i;i-2>0;i--)
 			putchar(' ');
-		for(;j<3+2*z;j++)
+        i=3;
+		for(j=0;j<3+2*z;j++)
 			putchar('*');
 	 	putchar('\n');
 	}
